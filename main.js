47053fef13b5d611c5a83bf3d44309e56a144ded
leahgwin <leahgwin@gmail.com>
2018-06-04 10:14:34 -0500

    correctly solved the practice and connected js and html

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 // Use JavaScript to obtain a reference to the first article header and change its text with textContent property to "Welcome the {insert your name here} blog"
-let heading = document.querySelector(".article__header").textContent= "Welcome to the Leah Blog";
+let heading = document.querySelector(".article__header");
+heading.textContent= "Welcome to the Leah Blog";
 
 // Use JavaScript to obtain a reference to all article__header elements and change their classList property value to "article__header important".
-let important = document.querySelectorAll(".article__header").classList.add("article__header important");
+let important = document.querySelectorAll(".article__header");
+
+for (i = 0; i < important.length; i++) { 
+important[i].classList.add("important");
+console.log(important[i].classList);
+}
